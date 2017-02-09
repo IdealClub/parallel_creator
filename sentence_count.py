4f64020d41f521755cc0d1ca34a5883b4e61ca95
adamcsvarga <adamcs.varga@gmail.com>
2017-02-09 09:21:53 +0100

    count last block too

--- sentence_count.py
+++ sentence_count.py
@@ -20,7 +20,7 @@ count_2 = 0
 last_j = 0
 for i, line_1 in enumerate(lines_1):
     if int(line_1.strip()) == 0 and zeros_passed:
-        count_1 += int(lines_1[i-1].strip())
+        count_1 += int(lines_1[i-1].strip()) + 1
         max_2 = 0
         for line_2 in lines_2[last_j:i]:
             if int(line_2.strip()) == 0 and max_2 != 0:
@@ -28,10 +28,21 @@ for i, line_1 in enumerate(lines_1):
             if int(line_2.strip()) > max_2:
                 max_2 = int(line_2.strip())
         last_j = i
-        count_2 += max_2
+        count_2 += max_2 + 1
         zeros_passed = False
     if int(line_1.strip()) != 0:
         zeros_passed = True
+        
+## last block
+count_1 += int(lines_1[:-1].strip()) + 1
+max_2 = 0
+for line_2 in lines_2[last_j:]:
+    if int(line_2.strip()) == 0 and max_2 != 0:
+        break
+    if int(line_2.strip()) > max_2:
+        max_2 = int(line_2.strip())
+count_2 += max_2 + 1
+
         
 print("Sentence count 1: "+str(count_1)+'\n')
 print("Sentence count 2: "+str(count_2)+'\n')
