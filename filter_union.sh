1903695220ce7468a2edffafa1ad6322e0463734
adamcsvarga <adamcs.varga@gmail.com>
2017-02-19 15:25:03 +0100

    unique union creator added

--- filter_union.sh
+++ filter_union.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Creates the (unique) union article title list for two matched ID files
-## Usage: filter_union.sh <file1> <file2>
+## Usage: filter_union.sh <file1> <file2> <outfile>
 
 while read line; do
 	title=`echo $line | cut -f2 -d$'\t'`
-	match=`cat $2 | grep $title`
+	match=`cat $2 | grep "$title"`
 	if [[ -z $match ]]; then
-		echo $line >> ${1}.${2}.uu
+		echo $line >> $3
 	fi
 done < $1	
