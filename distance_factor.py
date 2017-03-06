3d78cbf182e0a0f661c3aea1fd3de08c0ca58aad
adamcsvarga <adamcs.varga@gmail.com>
2017-03-06 10:42:38 +0100

    distance b\w source & target article sentence pairs

---
+++ distance_factor.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 10:26:07 2017
+
+@author: vurga
+"""
+
+import sys
+
+with open(sys.argv[1]+'.dist', 'a+') as target:
+    target.write(",dist\n")
+with open(sys.argv[1], 'r') as source:
+    lines = source.readlines()
+    
+distances = []
+for line in lines:
+    num_lines_source = line.strip().split()[0]
+    num_lines_target = line.strip().split()[1]
+    
+    ind = 0
+    for i in range(0, num_lines_source):
+        for j in range(0, num_lines_target):
+            distances.append(str(ind)+","+str(abs(i-j)))
+            ind += 1
+
+with open(sys.argv[1]+'.dist', 'a+') as target:
+    target.write('\n'.join(distances))
+            
+
