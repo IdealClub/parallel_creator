46a38f4ac003a01dd73fb348a2e216a92951808d
adamcsvarga <adamcs.varga@gmail.com>
2017-03-13 10:21:31 +0100

    training script added

--- create_training.py
+++ create_training.py
@@ -12,7 +12,7 @@ import numpy as np
 def code2index(code):
     return int(code.split('-')[1]) - 1
 
-with open('training.'+sys.argv[1], 'w'), open('training.'+sys.argv[2], 'w'):
+with open(sys.argv[1]+'.train', 'w'), open(sys.argv[2]+'.train', 'w'):
     pass
 
 with open(sys.argv[1], 'r') as source1, open(sys.argv[2], 'r') as source2, open(sys.argv[3],'r') as source3:
@@ -29,7 +29,7 @@ for line in lines_gold:
     matching[index1] = index2
     
 for key, value in matching.items():   
-    with open('training.'+sys.argv[1], 'a+') as target1, open('training.'+sys.argv[2], 'a+') as target2:
+    with open(sys.argv[1]+'.train', 'a+') as target1, open(sys.argv[2]+'.train', 'a+') as target2:
         target1.write(lines1[key].split()[1]+'\n')
         target2.write(lines2[value].split()[1]+'\n')
 
@@ -41,7 +41,7 @@ for i in range(0, len(lines_gold)):
         index1 = np.random.randint(0, len(lines1))
         index2 = np.random.randint(0, len(lines2))
     
-    with open('training.'+sys.argv[1], 'a+') as target1, open('training.'+sys.argv[2], 'a+') as target2:
+    with open(sys.argv[1]+'.train', 'a+') as target1, open(sys.argv[2]+'.train', 'a+') as target2:
         target1.write(lines1[index1].split()[1]+'\n')
         target2.write(lines2[index2].split()[1]+'\n')
     
