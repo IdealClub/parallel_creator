f355bd2d6f73da2a8a1bba0624bfd33d135c3c20
adamcsvarga <adamcs.varga@gmail.com>
2017-03-27 09:26:38 +0200

    classifier training w context vectors

--- train_classifier.py
+++ train_classifier.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     
     features = []
     labels = []
-    for arg in sys.argv[1:]:
+    for arg in sys.argv[1:3]:
         X = read_data(arg)
         pos = np.repeat(1, len(X)/2)
         neg = np.repeat(0, len(X)/2)
@@ -107,7 +107,15 @@ if __name__ == '__main__':
     train_labels.columns = ['label']
     training_data = pd.concat((train_instances, train_labels), axis=1)
     
-    print(len(training_data))
+    ## read context vector similarities
+    cs = []
+    for arg in sys.argv[3:]:
+        C = pd.read_csv(arg, header=None)
+        cs.append(C)
+        
+    cosine_sims = pd.concat(cs, ignore_index=True)
+    cosine_sims.columns = ['context-500k']
+    training_data = pd.concat((training_data, cosine_sims), axis=1)
     
     training_data = shuffle(training_data, random_state=3)
     sv = train_and_xval(training_data[:32666], a="svm")
