88379bf2faae6cd176f2b2a9e29ada5743dccbb0
adamcsvarga <adamcs.varga@gmail.com>
2017-05-10 11:56:12 +0200

    nn extractor

--- feature_extract_nn.py
+++ feature_extract_nn.py
@@ -54,10 +54,10 @@ with open(sys.argv[1], 'r') as corpusA, open(sys.argv[2]) as corpusB, open(sys.a
                     cvec_1 = np.fromstring(contxA[i].strip(), sep=" ")
                     cvec_2 = np.fromstring(contxB[j].strip(), sep=" ")
                     ## compute cosine sim
-                    feas_conc = np.concatenate((cvec_1, cvec_2), axis=1)
+                    feas_conc = np.concatenate((cvec_1, cvec_2))
                     feas_mult = np.multiply(cvec_1, cvec_2)
                     feas_subt = cvec_1 - cvec_2
-                    feas = np.concatenate((feas_conc, feas_mult, feas_subt), axis=1)
+                    feas = np.concatenate((feas_conc, feas_mult, feas_subt))
                         
                     if True:
                         pred = model.predict(feas)[1]
