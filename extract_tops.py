94b9bc2356fad9fddaecb472b79d033a5afe6942
adamcsvarga <adamcs.varga@gmail.com>
2017-07-07 18:56:39 +0200

    extract top parallel and discard untranslated sentences

--- extract_tops.py
+++ extract_tops.py
@@ -39,6 +39,8 @@ def compare_snt(a, b, lan1, lan2):
     #print(a, b)
     
     if (len(a) or len(b)) and (1 - (editdistance.eval(a, b) / max(len(a), len(b))) < .9):
+        print(a, b)
+        print(editdistance.eval(a, b) / max(len(a), len(b)))
         return True
     return False
     
